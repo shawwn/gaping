54edb9523ab560fe458170fd692869d0c75692ef
Shawn Presser <shawnpresser@gmail.com>
2021-01-03 19:24:06 -0800

    WIP CI tests

--- gaping/gaping_test.py
+++ gaping/gaping_test.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path += [os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')]
 import tensorflow as tf
 
 from gaping.tpu_topology_test import *
--- gaping/gaping_test_ci.py
+++ gaping/gaping_test_ci.py
@@ -1,4 +1,26 @@
+import os
+import sys
+sys.path += [os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')]
 import tensorflow as tf
+
+
+from absl import flags
+from absl.testing import parameterized
+
+from gaping import test_utils
+import tensorflow_hub as hub
+
+
+class GapingTestCI(parameterized.TestCase, test_utils.GapingTestCase):
+
+  def setUp(self):
+    super().setUp()
+
+  @parameterized.parameters([42,99])
+  def testBasic(self, value):
+    print('cwd: {!r}'.format(os.getcwd()))
+    print('value: {}'.format(value))
+
 
 if __name__ == "__main__":
   import gaping.wrapper
