6448c7c1d7e5de688e6a6f57d71d421b5f1ff199
Shawn Presser <shawnpresser@gmail.com>
2021-01-29 05:52:18 -0800

    WIP resnet: add Bottleneck

--- gaping/models/resnet.py
+++ gaping/models/resnet.py
@@ -43,7 +43,7 @@ class BasicBlock(nn.Module):
         base_width: int = 64,
         dilation: int = 1,
         norm_layer: Optional[Callable[..., nn.Module]] = None,
-        scope: str = 'layer',
+        scope: str = 'block',
         **kws,
     ) -> None:
         super(BasicBlock, self).__init__(scope=scope, **kws)
@@ -73,6 +73,69 @@ class BasicBlock(nn.Module):
 
             out = self.conv2(out)
             out = self.bn2(out)
+
+            if self.downsample is not None:
+                identity = self.downsample(x)
+
+            out += identity
+            out = self.relu(out)
+
+            return out
+
+
+
+class Bottleneck(nn.Module):
+    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
+    # This variant is also known as ResNet V1.5 and improves accuracy according to
+    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+
+    expansion: int = 4
+
+    def __init__(
+        self,
+        inplanes: int,
+        planes: int,
+        stride: int = 1,
+        downsample: Optional[nn.Module] = None,
+        groups: int = 1,
+        base_width: int = 64,
+        dilation: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = None,
+        scope: str = 'bottleneck',
+        **kws
+    ) -> None:
+        super(Bottleneck, self).__init__(scope=scope, **kws)
+        with self.scope():
+            if norm_layer is None:
+                norm_layer = nn.BatchNorm2d
+            width = int(planes * (base_width / 64.)) * groups
+            # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+            self.conv1 = conv1x1(inplanes, width, scope='conv1')
+            self.bn1 = norm_layer(width, scope='bn1')
+            self.conv2 = conv3x3(width, width, stride, groups, dilation, scope='conv2')
+            self.bn2 = norm_layer(width, scope='bn2')
+            self.conv3 = conv1x1(width, planes * self.expansion, scope='conv3')
+            self.bn3 = norm_layer(planes * self.expansion, scope='bn3')
+            self.relu = nn.ReLU(inplace=True, scope='relu')
+            self.downsample = downsample
+            self.stride = stride
+
+    def forward(self, x: Tensor) -> Tensor:
+        with self.scope():
+            identity = x
+
+            out = self.conv1(x)
+            out = self.bn1(out)
+            out = self.relu(out)
+
+            out = self.conv2(out)
+            out = self.bn2(out)
+            out = self.relu(out)
+
+            out = self.conv3(out)
+            out = self.bn3(out)
 
             if self.downsample is not None:
                 identity = self.downsample(x)
