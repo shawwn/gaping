f790ea9c48d67674b8a94cf9401b5782cbe90bb9
Shawn Presser <shawnpresser@gmail.com>
2021-03-16 04:03:34 -0700

    WIP biggan_train.py

--- gaping/biggan_train.py
+++ gaping/biggan_train.py
@@ -43,7 +43,10 @@ from . import tftorch as nn
 
 from .optim import optimizer
 
+import tqdm
+
 import tensorflow as tf
+import numpy as np
 
 def loss_hinge_dis(dis_fake, dis_real):
   loss_real = nn.mean(nn.relu(1. - dis_real))
@@ -58,6 +61,16 @@ generator_loss = loss_hinge_gen
 discriminator_loss = loss_hinge_dis
 
 
+def stop(*args):
+  def fn(x):
+    if callable(x):
+      x = x()
+    return tf.stop_gradient(x)
+  if len(args) <= 1:
+    return fn(args[0])
+  else:
+    return [fn(x) for x in args]
+
 def for_(n, body, *args, back_prop=False, parallel_iterations=1, **kws):
   results = tf.while_loop(
       lambda *args: True,
@@ -69,37 +82,81 @@ def for_(n, body, *args, back_prop=False, parallel_iterations=1, **kws):
       **kws)
   return results
 
+class G_D(nn.Module):
+  def __init__(self, G, D, scope='', **kws):
+    super().__init__(scope=scope, **kws)
+    self.G = G
+    self.D = D
+
+  def forward(self, z, y, x=None, dy=None, train_G=False, return_G_z=False, split_D=True):
+    # Get Generator output given noise
+    G_z = self.G(z, y)
+    # If not training G, disable gradients
+    if not train_G:
+      G_z = stop(G_z)
+    # Split_D means to run D once with real data and once with fake,
+    # rather than concatenating along the batch dimension.
+    if split_D:
+      D_fake = self.D(G_z, y)
+      if x is not None:
+        D_real = self.D(x, dy)
+        return D_fake, D_real
+      else:
+        if return_G_z:
+          return D_fake, G_z
+        else:
+          return D_fake
+    else:
+      raise NotImplementedError()
+
 
 class TrainGAN(biggan.GAN):
-  def __init__(self, options):
+  def __init__(self, options, features):
     super().__init__()
+    self.GD = G_D(self.gan.generator, self.gan.discriminator)
     self.G_optim = optimizer.SGD(self.gan.generator.parameters(), lr=options.G_lr)
     self.D_optim = optimizer.SGD(self.gan.discriminator.parameters(), lr=options.D_lr)
 
-  def sample_z(self, batch_size=None):
+    self.train_op = tf.tpu.batch_parallel(
+        lambda image: self.model_step({'image': image}),
+        inputs=[features.image],
+        num_shards=1,
+        device_assignment=driver_lib.get().device_assignment([0]),
+        )[0]
+
+  @property
+  def num_classes(self):
+    return self.gan.generator.n_class
+
+  def sample_z(self, batch_size=1):
     shape = [self.gan.generator.dim_z]
     if batch_size is not None:
       shape = [batch_size] + shape
-    return nn.truncated_normal(shape, mean=0.0, std=1.0)
+    return stop(nn.truncated_normal(shape, mean=0.0, std=1.0))
 
-  def sample_y(self, batch_size=None):
+  def sample_y(self, batch_size=1, maxval=1):
     shape = []
     if batch_size is not None:
       shape = [batch_size] + shape
-    labels = tf.random.uniform(shape, minval=0, maxval=self.gan.generator.n_class, dtype=tf.int32)
-    return tf.one_hot(labels, self.gan.generator.n_class)
+    if maxval is None:
+      maxval = self.gan.generator.n_class
+    labels = tf.random.uniform(shape, minval=0, maxval=maxval, dtype=tf.int32)
+    return stop(tf.one_hot(labels, self.gan.generator.n_class))
 
   def model_fn(self, features, *args):
     features = EasyDict(features)
 
     # Zero G's gradients by default before training G, for safety
     with tf.control_dependencies(self.G_optim.zero_grad()):
+
+      # If accumulating gradients, loop multiple times before an optimizer step
+
       
       # If accumulating gradients, loop multiple times
       if True:
         def train_G_body(accumulation_index):
-          z = self.sample_z(1)
-          y = self.sample_y(1)
+          z = self.sample_z()
+          y = self.sample_y()
           D_fake = self.gan.generator(z, y)
           G_loss = generator_loss(D_fake) / float(options.num_G_accumulations)
           with tf.control_dependencies(self.G_optim.backward(G_loss)):
@@ -110,9 +167,9 @@ class TrainGAN(biggan.GAN):
         ops = []
         for accumulation_index in range(options.num_G_accumulations):
           with tf.control_dependencies(ops):
-            z = self.sample_z(1)
-            y = self.sample_y(1)
-            D_fake = self.gan.generator(z, y)
+            z = self.sample_z()
+            y = self.sample_y()
+            D_fake = self.GD(z, y, train_G=True)
             G_loss = generator_loss(D_fake) / float(options.num_G_accumulations)
             ops.extend(self.G_optim.backward(G_loss))
 
@@ -122,6 +179,31 @@ class TrainGAN(biggan.GAN):
     with tf.control_dependencies(ops):
       return tf.no_op()
 
+  def model_step(self, features):
+    return self.model_fn(features)
+
+  def run(self, op):
+    driver = driver_lib.get()
+    return driver.session.run(op)
+
+  def fit(self):
+    print('Initializing...')
+    #self.run(self.gan.initializer)
+
+    gs = tf.train.get_or_create_global_step()
+
+    i = gs.initialized_value().eval()
+    print('Training from step {}...'.format(i))
+    while True:
+      result = self.run(self.train_op)
+      print(
+          'Step {}'.format(i),
+          self.GD.G.ScaledCrossReplicaBN.bn.num_batches_tracked.eval(),
+          self.GD.G.ScaledCrossReplicaBN.bn.running_mean.eval(),
+          )
+      i += 1
+      gs.load(i)
+
 def main(opts):
   globals()['options'] = opts
 
@@ -130,23 +212,15 @@ def main(opts):
 
   features = nxt
 
-  trainer = TrainGAN(options)
+  trainer = TrainGAN(options=options, features=features)
 
-  #train_op = trainer.model_fn(features)
-
-  train_op = tf.tpu.batch_parallel(
-      lambda image: trainer.model_fn({'image': image}),
-      inputs=[nxt.image],
-      num_shards=1,
-      device_assignment=driver.device_assignment([0])
-      )[0]
+  trainer.fit()
   
-
-  import pdb; pdb.set_trace()
   assert False
   assert False
 
 if __name__ == '__main__':
+  np.set_printoptions(suppress=True)
   parser = util.prepare_parser()
   util.run_app(parser, main)
 
